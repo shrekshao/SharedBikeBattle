f4730e9477f1c3e7cecefb0fd9351b45cf250e4d
shrekshao <shrekshao@gmail.com>
2017-06-28 23:24:40 +0800

    first commit setup

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+    Animator m_animator;
+
+	// Use this for initialization
+	void Start () {
+        m_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space))
+        {
+            m_animator.SetTrigger("attack");
+        }
+	}
+}
