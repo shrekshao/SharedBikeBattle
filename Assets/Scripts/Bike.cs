252898d37a677a80cf954e1206fb5165445a5d53
shrekshao <shrekshao@gmail.com>
2017-07-05 01:18:14 +0800

    ride bike

---
+++ Assets/Scripts/Bike.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bike : MonoBehaviour {
+
+    public Transform sitPivot;
+    //public GameObject paddle;
+    public Anima2D.IkLimb2D Lfoot;
+    public Anima2D.IkLimb2D Rfoot;
+    public Anima2D.IkLimb2D Lhand;
+
+    [SerializeField]
+    GameObject paddle;
+
+    [SerializeField]
+    GameObject wheelF;
+
+    [SerializeField]
+    GameObject wheelB;
+
+
+    Rigidbody m_rigidBody;
+
+    float maxSpeedHorizontal = 10f;
+    float maxSpeedVertical = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public void Ride(float hacc, float vacc)
+    {
+        Vector3 bikeVelocity = m_rigidBody.velocity;
+
+        bikeVelocity.x += hacc;
+        bikeVelocity.z += vacc;
+
+        bikeVelocity.x = Mathf.Clamp(bikeVelocity.x, -maxSpeedHorizontal, maxSpeedHorizontal);
+        bikeVelocity.z = Mathf.Clamp(bikeVelocity.z, -maxSpeedVertical, maxSpeedVertical);
+
+        m_rigidBody.velocity = bikeVelocity;
+
+
+        // bike facing
+        if (Mathf.Abs(bikeVelocity.x) > 0.001f)
+        {
+            //transform.localScale = new Vector3(Mathf.Sign(bikeVelocity.x), 1f, 1f);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * Mathf.Sign(bikeVelocity.x), transform.localScale.y, transform.localScale.z);
+        }
+
+        float r = bikeVelocity.x * 60f * Time.deltaTime;
+        wheelF.transform.Rotate(Vector3.back, r, Space.Self);
+        wheelB.transform.Rotate(Vector3.back, r, Space.Self);
+        paddle.transform.Rotate(Vector3.back, r, Space.Self);
+    }
+}
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,11 +6,25 @@ public class Character : MonoBehaviour {
 
     Animator m_animator;
     Rigidbody m_rigidBody;
+    UnityEngine.Rendering.SortingGroup m_sortingGroup;
+
+    bool riding = false;
+
+    Bike bike = null;
+
+    public Anima2D.Bone2D Lfoot;
+    public Anima2D.Bone2D Rfoot;
+    public Anima2D.Bone2D Lhand;
+
+    // temp for bike
+    float maxSpeedHorizontal = 10f;
+    float maxSpeedVertical = 3f;
 
     // Use this for initialization
     void Start () {
         m_animator = GetComponent<Animator>();
         m_rigidBody = GetComponent<Rigidbody>();
+        m_sortingGroup = GetComponent<UnityEngine.Rendering.SortingGroup>();
     }
 	
 	// Update is called once per frame
@@ -18,21 +32,64 @@ public class Character : MonoBehaviour {
         float hacc = Input.GetAxis("Horizontal");
         float vacc = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(hacc) > 0 || Mathf.Abs(vacc) > 0)
+        if (!riding)
         {
-            m_animator.SetBool("walking", true);
+            // walking
+            if (Mathf.Abs(hacc) > 0 || Mathf.Abs(vacc) > 0)
+            {
+                m_animator.SetBool("walking", true);
+            }
+            else
+            {
+                m_animator.SetBool("walking", false);
+            }
+
+            transform.position += (3.0f * Vector3.right * hacc + Vector3.forward * vacc) * Time.deltaTime ;
+
+            if (Mathf.Abs(hacc) > 0.001f)
+            {
+                //transform.localScale = new Vector3(Mathf.Sign(bikeVelocity.x), 1f, 1f);
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * Mathf.Sign(hacc), transform.localScale.y, transform.localScale.z);
+            }
         }
         else
         {
-            m_animator.SetBool("walking", false);
+            // riding
+
+            bike.Ride(hacc, vacc * 0.3f);
         }
 
-        transform.position += Vector3.right * hacc * Time.deltaTime;
+        
+    }
 
-        if (Mathf.Abs(hacc) > 0.001f)
+
+
+    void OnTriggerStay(Collider c)
+    {
+        if (c.CompareTag("Bike"))
         {
-            //transform.localScale = new Vector3(Mathf.Sign(bikeVelocity.x), 1f, 1f);
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * Mathf.Sign(hacc), transform.localScale.y, transform.localScale.z);
+            if(Input.GetKeyUp(KeyCode.Z))
+            {
+                Debug.Log("ride bike");
+
+                Bike bike = c.gameObject.GetComponent<Bike>();
+                m_sortingGroup.enabled = false;
+                transform.parent = c.transform;
+                transform.localPosition = bike.sitPivot.localPosition;
+                bike.Lfoot.enabled = true;
+                bike.Lfoot.target = Lfoot;
+                bike.Rfoot.enabled = true;
+                bike.Rfoot.target = Rfoot;
+                bike.Lhand.enabled = true;
+                bike.Lhand.target = Lhand;
+
+                riding = true;
+                m_animator.SetBool("riding", true);
+                this.bike = bike;
+
+                GetComponent<Collider>().enabled = false;
+            }
         }
     }
+
 }
